685b663216d6953f91242d9037b2ff4917f6f109
Lucas Mendes <lucaslsbh1@gmail.com>
2020-09-08 07:41:19 -0300

    Using my own implementation of array in Binary Search algorithm

--- algorithms/binary_search.c
+++ algorithms/binary_search.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "../data-structures/array/array.h"
+
 /**
  * Search a sorted array by repeatedly dividing the search interval in half.
  * Begin with an interval covering the whole array. If the value of the search key is less
@@ -9,13 +11,13 @@
  * Time complexity: O(Log n)
  */
 
-int binary_search(int arr[], int element, int size) {
+int binary_search(array_t arr, array_element element) {
   int left = 0;
-  int right = size -1;
+  int right = arr.length -1;
 
   while (left <= right) {
     int middle = (left + right) / 2;
-    int guess = arr[middle];
+    int guess = array_get(&arr, middle);
 
     if (guess == element) {
       return middle;
@@ -33,15 +35,24 @@ int binary_search(int arr[], int element, int size) {
 }
 
 int main() {
-  int items[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
-  int size = sizeof(items) / sizeof(items[0]);
+  array_t items = array_create(10);
+  array_push(&items, 2);  // index: 0
+  array_push(&items, 4);  // index: 1
+  array_push(&items, 6);  // index: 2
+  array_push(&items, 8);  // index: 3
+  array_push(&items, 10); // index: 4
+  array_push(&items, 12); // index: 5
+  array_push(&items, 14); // index: 6
+  array_push(&items, 16); // index: 7
+  array_push(&items, 18); // index: 8
+  array_push(&items, 20); // index: 9
 
-  int pos = binary_search(items, 12, size);
+  int pos = binary_search(items, 12);
   printf("Position of number 12: %d\n", pos);
 
-  pos = binary_search(items, 20, size);
+  pos = binary_search(items, 20);
   printf("Position of number 20: %d\n", pos);
 
-  pos = binary_search(items, 0, size);
+  pos = binary_search(items, 0);
   printf("Position of number 0: %d\n", pos);
 }
